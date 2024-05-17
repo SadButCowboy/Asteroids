3d7cc47aea1b201e7dfb5850d10905fdcd693447
SadButCowboy <112869796+SadButCowboy@users.noreply.github.com>
2024-05-17 12:40:29 +0200

    Spring somewhat implemented

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module Core {
     requires Common;
     requires CommonBullet;    
-    requires javafx.graphics;    
-    opens dk.sdu.mmmi.cbse.main to javafx.graphics;
+    requires javafx.graphics;
+    requires spring.context;
+    requires spring.beans;
+    requires spring.core;
+    opens dk.sdu.mmmi.cbse.main to javafx.graphics,spring.core;
+    exports dk.sdu.mmmi.cbse.main;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
